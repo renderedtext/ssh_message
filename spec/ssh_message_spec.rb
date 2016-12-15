c5bba92b61de31645b81ad17d0ae6a4e8ffdb59b
Igor Šarčević <igor@renderedtext.com>
2016-12-15 11:19:55 +0100

    Write tests for line length

--- spec/ssh_message_spec.rb
+++ spec/ssh_message_spec.rb
@@ -5,18 +5,22 @@ describe SshMessage do
     expect(SshMessage::VERSION).not_to be nil
   end
 
-  it "composes an ssh message" do
-    info = {
-      :kernel => "Linux 3.1.2",
-      :user => "runner",
-      :project_name => "semaphore",
-      :branch_name => "is/pretty_ssh_message",
-      :commit_sha => "b50069befccb8e6c31f14c142af96699101b4ee3",
-      :build_number => 23,
-      :job_count => 64,
-      :job_index => 12
-    }
+  def without_colors(string)
+    string.gsub(/\e\[\d+m/, "")
+  end
 
+  let(:info) {{
+    :kernel => "Linux 3.1.2",
+    :user => "runner",
+    :project_name => "semaphore",
+    :branch_name => "is/pretty_ssh_message",
+    :commit_sha => "b50069befccb8e6c31f14c142af96699101b4ee3",
+    :build_number => 23,
+    :job_count => 64,
+    :job_index => 12
+  }}
+
+  it "composes an ssh message" do
 expected_message = <<HERE
            *****      *****
           ******************                Welcome to Semaphore's SSH session
@@ -30,11 +34,11 @@ expected_message = <<HERE
     **********          **********          Build Number: 23
     **********          **********          Job: 12/64
     **********          **********
-   ***********         ************         Lorem ipsum dolor sit amet, ex sea possim probatus. Et ignota discere
- ***********    *********************       his. Nam partem facete cu. Molestie appetere accommodare has ex,
-  *********   **********************        velit deseruisse est ex, audire inimicus mei cu. Vix nobis discere
-   ******    **********************         eu, augue patrioque nam no. Audire sanctus at sit, eum no veri
-           *******************              homero. Ex cotidieque adversarium vis, in sint fierent
+   ***********         ************         Lorem ipsum dolor sit amet, ex sea possim probatus.
+ ***********    *********************       Et ignota discere his. Nam partem facete cu. Molestie
+  *********   **********************        velit deseruisse est ex, audire inimicus mei cu.
+   ******    **********************         eu, augue patrioque nam no. Audire sanctus at sit,
+           *******************              homero. Ex cotidieque adversarium vis, in sint
           ******************                corrumpit mel, an qui civibus accusamus. Ius id stet
           ******************                copiosae. Ad pri omnes graeci.
          *******************
@@ -46,7 +50,12 @@ HERE
     expect(message).to eq(expected_message)
   end
 
-  def without_colors(string)
-    string.gsub(/\e\[\d+m/, "")
+  it "keeps the lenght of lines under 100 characters" do
+    message_lines = without_colors(SshMessage.compose(info)).split("\n")
+
+    message_lines.each do |line|
+      puts line
+      expect(line.size).to be < 100
+    end
   end
 end
